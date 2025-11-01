b43811a032505c4a750b2205450851f9cfade688
Alex <sashaaslecs@gmail.com>
2025-11-01 20:42:53 +0300

    bot

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
