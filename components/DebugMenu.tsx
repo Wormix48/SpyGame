daefef3cb947e41aac01075b61638c8e4d787eda
Alex <sashaaslecs@gmail.com>
2025-11-01 20:12:07 +0300

    fix3

--- components/DebugMenu.tsx
+++ components/DebugMenu.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { GameState, Player, Answer, Vote, ChatMessage } from '../types';
 import { db } from '../firebase';
-// FIX: Switched to Firebase v8 compatibility API.
-import firebase from 'firebase/compat/app';
+import firebase from 'firebase/app';
+import 'firebase/database'; // Import database module for ServerValue.TIMESTAMP
 import { generateId } from '../utils';
 import { Avatar } from './Avatar';
 
