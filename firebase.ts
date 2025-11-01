c53f39546464e8df94ecf5be13c5fc02003e00d0
Alex <sashaaslecs@gmail.com>
2025-11-01 20:13:24 +0300

    fix4

--- firebase.ts
+++ firebase.ts
@@ -1,9 +1,9 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
 
 
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+const app = firebase.initializeApp(firebaseConfig);
+export const db = firebase.database();
